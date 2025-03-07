538aa17351c3752dd798f12dbb3a551bdf90d3b6
multimatic-ferdin <fnorbert@multimatic.com>
2025-03-07 11:57:05 -0500

    input validation

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,29 +1,13 @@
 "use client";
 
+import {
+  EyeFilledIcon,
+  EyeSlashFilledIcon,
+} from "@/components/FormComponents/EyeSlashFilledIcon";
 import { Form, Input, SelectItem, Select, Button } from "@heroui/react";
-import { useForm, SubmitHandler } from "react-hook-form";
 import { useState } from "react";
 
-type Inputs = {
-  companyName: string;
-  adminEmail: string;
-  companyPhone: string;
-  password: string;
-  confirmPassword: string;
-  industryType: string;
-  employeeCount: number;
-};
 export default function OnBoarding() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-    reset,
-  } = useForm<Inputs>();
-
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
   const industryTypes = [
     { key: "Engineering", label: "Engineering" },
     { key: "Healthcare", label: "Healthcare" },
@@ -45,8 +29,41 @@ export default function OnBoarding() {
     { key: "Government", label: "Government" },
     { key: "Non-Profit", label: "Non-Profit" },
   ];
-  const password = watch("password", "");
-  const [errors, setErrors] = useState({});
+
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const togglePasswordVisibility = () =>
+    setIsPasswordVisible(!isPasswordVisible);
+  const [isConfPasswordVisible, setIsConfPasswordVisible] = useState(false);
+  const toggleConfPasswordVisibility = () =>
+    setIsConfPasswordVisible(!isConfPasswordVisible);
+  const errors: string[] = [];
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data: Record<string, FormDataEntryValue> = Object.fromEntries(
+      new FormData(e.currentTarget)
+    );
+    console.log(data);
+  };
+
+  const validatePasswords = () => {
+    if (password.length < 4) {
+      errors.push("Password must be 4 characters or more.");
+    }
+    if ((password.match(/[A-Z]/g) || []).length < 1) {
+      errors.push("Password must include at least 1 upper case letter");
+    }
+    if ((password.match(/[^a-z0-9]/gi) || []).length < 1) {
+      errors.push("Password must include at least 1 symbol.");
+    }
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+    } else {
+      setError("");
+    }
+  };
   return (
     <div className="h-screen w-screen flex items-center justify-center font-sans">
       <div className="flex flex-col gap-3 w-96 rounded-md p-6 bg-white shadow-md">
@@ -55,91 +72,77 @@ export default function OnBoarding() {
         </h1>
         <h2 className="text-center">Tell Us About Your Company</h2>
         <p></p>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            label="Company name"
-            type="text"
-            isRequired
-            {...register("companyName")}
-          />
-          <Input
-            label="Admin email"
-            type="email"
-            {...register("adminEmail", {
-              required: "Email is required",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: "Invalid email address",
-              },
-            })}
-            isRequired
-          />
-          <Input
-            label="Company Phone"
-            type="text"
-            {...register("companyPhone", {
-              pattern: {
-                value: /^[0-9+-\s()]*$/,
-                message: "Invalid phone number format",
-              },
-            })}
-          />
+        <Form onSubmit={onSubmit}>
+          <Input label="Company name" name="companyName" isRequired />
+          <Input label="Admin email" isRequired type="email" />
+          <Input label="Company Phone" type="text" />
           <Input
             label="Type a strong password"
-            type="password"
-            isRequired
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 8,
-                message: "Password must be at least 8 characters",
-              },
-              pattern: {
-                value:
-                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                message:
-                  "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character",
-              },
-            })}
+            type={isPasswordVisible ? "text" : "password"}
+            endContent={
+              <button
+                aria-label="toggle password visibility"
+                className="focus:outline-none"
+                type="button"
+                onClick={togglePasswordVisibility}
+              >
+                {isPasswordVisible ? (
+                  <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                ) : (
+                  <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                )}
+              </button>
+            }
+            errorMessage={() => (
+              <ul>
+                {errors.map((error, i) => (
+                  <li key={i}>{error}</li>
+                ))}
+              </ul>
+            )}
+            isInvalid={errors.length > 0}
+            value={password}
+            onValueChange={setPassword}
+            onBlur={validatePasswords}
           />
           <Input
             label="Confirm your passowrd"
-            type="password"
-            {...register("confirmPassword", {
-              required: "Please confirm your password",
-              validate: (value) =>
-                value === password || "Passwords do not match",
-            })}
-            isRequired
+            type={isConfPasswordVisible ? "text" : "password"}
+            endContent={
+              <button
+                aria-label="toggle password visibility"
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleConfPasswordVisibility}
+              >
+                {isConfPasswordVisible ? (
+                  <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                ) : (
+                  <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                )}
+              </button>
+            }
+            value={confirmPassword}
+            onValueChange={setConfirmPassword}
+            onBlur={validatePasswords}
+            isInvalid={!!error}
+            errorMessage={error}
           />
           <Select label="Select Industry Type" isRequired>
             {industryTypes.map((types) => (
               <SelectItem key={types.key}>{types.label}</SelectItem>
             ))}
           </Select>
-          <input
-            type="hidden"
-            {...register("industryType", {
-              required: "Industry type is required",
-            })}
-          />
           <Input
             label="Number of Employees to add to this application"
             type="number"
             isRequired
-            {...register("employeeCount", {
-              valueAsNumber: true,
-              min: {
-                value: 1,
-                message: "Must have at least 1 employee",
-              },
-            })}
           />
           <div className="flex gap-2 mx-auto mt-2">
             <Button className="bg-black text-white" type="submit">
               Next
             </Button>
-            <Button type="reset" variant="flat" onPress={() => reset()}>
+            <Button type="reset" variant="flat">
               Reset
             </Button>
           </div>
